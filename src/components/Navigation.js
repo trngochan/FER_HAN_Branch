5cf38742ee8065e3c5e5651ee794d4ce10393f06
trngochan <85632212+trngochan@users.noreply.github.com>
2023-06-07 11:51:56 +0700

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+export default class Navigation extends Component {
+    render() {
+        return (
+            <div className="header" >
+                <nav>
+                    <ul>
+                        <li><a className="active" href='#home'>Home</a></li>
+                        <li><a className="active" href='#about'>About</a></li>
+                        <li><a className="active" href='#news'>News</a></li>
+                        <li><a className="active" href='#contact'>Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
