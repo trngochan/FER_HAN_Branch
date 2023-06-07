701f93823f9321443c7575818497832cb3a69738
trngochan <85632212+trngochan@users.noreply.github.com>
2023-06-07 11:58:51 +0700

    h

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import React, { Component } from 'react';
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className='footer'>
+                <p>Copyright <i class="fa-regular fa-copyright"></i> 2022</p>
+            </div >
+        )
+    }
+}
