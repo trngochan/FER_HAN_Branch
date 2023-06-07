d7157acecd500d4f7f2d26017f3e2546f061a2ce
trngochan <85632212+trngochan@users.noreply.github.com>
2023-06-07 11:57:31 +0700

    h

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Players from './components/Players';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Players />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Players.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Players } from '../shared/ListOfPlayers';
+export default class players extends Component {
+
+    render() {
+        return (
+            <div className='container' >
+                {
+                    Players.map((player) => (
+                        <div className='column'>
+                            <div className='card'>
+                                <img src={player.img} />
+                                <h3>{player.name}</h3>
+                                <p className='title'>{player.club}</p>
+                                <p><button>Detail</button></p>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+        )
+    }
+}
